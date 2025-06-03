fd782cb6ab3ced79cdb8b53966d480a533c2eea2
Lachicagladiadora <dev.roseny@gmail.com>
2025-06-03 11:30:27 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "./atomic/atoms/Logo";
+
+function App() {
+  return (
+    <div className="h-full min-h-[100dvh] flex items-center justify-center bg-white">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
